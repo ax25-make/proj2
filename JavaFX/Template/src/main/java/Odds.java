c9957326bfc8897a0044f3bf2e6d65ee2355d3a7
ax25-make <xualex956@gmail.com>
2025-10-26 01:34:31 -0500

    dddd

--- JavaFX/Template/src/main/java/Odds.java
+++ JavaFX/Template/src/main/java/Odds.java
@@ -2,13 +2,14 @@ import javafx.scene.control.Alert;
 
 public class Odds {
 
-	public static void showOdds() {
+    public static void showOdds() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Keno Odds");
-        alert.setHeaderText("Keno Game Odds");
-        alert.setContentText("Keno Payout Odds Table:\n" +
+        alert.setHeaderText("Overall Odds of Winning");
+        alert.setContentText("1 Spot: 1 in 4\n" +
+                        "4 Spots: 1 in 3.86\n" +
+                        "8 Spots: 1 in 9.77\n" +
+                        "10 Spots: 1 in 9.05");
+        alert.showAndWait();
     }
 }
-
-///call rn
-/// 
--- JavaFX/Template/src/main/java/Welcome.java
+++ JavaFX/Template/src/main/java/Welcome.java
@@ -32,47 +32,47 @@ public class Welcome extends Application {
 		launch(args);
 	}
 
-	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
 		primaryStage.setTitle("Start Page");
 
-		// menu
+		// ---- MENU ----
 		MenuBar menuBar = new MenuBar();
 		Menu menu = new Menu("Menu");
 
 		MenuItem rulesItem = new MenuItem("Rules");
-        rulesItem.setOnAction(e -> Rules.showRules());
+		rulesItem.setOnAction(e -> Rules.showRules());
 
-        MenuItem oddsItem = new MenuItem("Odds");
-        //oddsItem.setOnAction(e -> showOdds());
+		MenuItem oddsItem = new MenuItem("Odds");
+		oddsItem.setOnAction(e -> Odds.showOdds());
 
-        MenuItem exitItem = new MenuItem("Exit");
-        exitItem.setOnAction(e -> primaryStage.close());
+		MenuItem exitItem = new MenuItem("Exit");
+		exitItem.setOnAction(e -> primaryStage.close());
 
-        menu.getItems().addAll(rulesItem, oddsItem, exitItem);
-        menuBar.getMenus().add(menu);
+		menu.getItems().addAll(rulesItem, oddsItem, exitItem);
+		menuBar.getMenus().add(menu);
 
-		// text
+		// ---- TEXT ----
 		Label text1 = new Label("ALEX'S WORLD FAMOUS");
 		Label text2 = new Label("KENO");
 		Label text3 = new Label("GAME");
-		text1.setStyle("-fx-text-fill: black; -fx-font-size: 18px;"); // "ALEX'S WORLD FAMOUS"
-		text3.setStyle("-fx-text-fill: black; -fx-font-size: 18px;"); // "GAME"
+
+		text1.setStyle("-fx-text-fill: black; -fx-font-size: 18px;");
 		text2.setStyle("-fx-text-fill: black; -fx-font-size: 100px;");
-		VBox textBox = new VBox(1, text1, text2, text3); // With Spacing of 10
-		
+		text3.setStyle("-fx-text-fill: black; -fx-font-size: 18px;");
 
+		VBox textBox = new VBox(5, text1, text2, text3);
+		textBox.setAlignment(Pos.CENTER);
 
+		// ---- IMAGE ----
+		Image image = new Image("https://img.freepik.com/premium-photo/red-board-with-red-white-game-middle-it_1354347-1648.jpg?w=360"); 
+		ImageView imageView = new ImageView(image);
+		imageView.setFitHeight(400);
+		imageView.setFitWidth(500);
+		imageView.setPreserveRatio(true);
+
+		// ---- BUTTON ----
 		Button startButton = new Button("START");
-		// startButton.setOnAction(e -> {
-        //     setupGameScene(); // create game scene
-        //     window.setScene(gameScene);
-        // });
-		Label startText = new Label("LOOSE ALL YOUR MONEY");
-		VBox buttonBox = new VBox(10, startText, startButton); // With Spacing of 10
-
 		startButton.setStyle(
 			"-fx-background-color: white;" +   
 			"-fx-text-fill: black;" +           
@@ -85,6 +85,7 @@ public class Welcome extends Application {
 			"-fx-padding: 10 30 10 30;"
 		);
 
+		Label startText = new Label("LOOSE ALL YOUR MONEY");
 		startText.setStyle(
 			"-fx-text-fill: black;" +
 			"-fx-font-size: 15px;" +
@@ -92,56 +93,21 @@ public class Welcome extends Application {
 			"-fx-padding: 10px;"
 		);
 
-		// FileInputStream inputstream = new FileInputStream("https://img.freepik.com/premium-photo/red-board-with-red-white-game-middle-it_1354347-1648.jpg?w=360"); 
-		Image image = new Image("https://img.freepik.com/premium-photo/red-board-with-red-white-game-middle-it_1354347-1648.jpg?w=360"); 
-		ImageView imageView = new ImageView(image);
-		//Setting the position of the image 
-		imageView.setX(50); 
-		imageView.setY(25); 
-		//setting the fit height and width of the image view 
-		imageView.setFitHeight(455); 
-		imageView.setFitWidth(500); 
-		//Setting the preserve ratio of the image view 
-		imageView.setPreserveRatio(true);  
-
-		textBox.setAlignment(Pos.CENTER);
+		VBox buttonBox = new VBox(10, startText, startButton);
 		buttonBox.setAlignment(Pos.CENTER);
-		textBox.setPadding(new Insets(0,0,10,0));
-		buttonBox.setPadding(new Insets(0,0,50,0));
 
-
-		// button1.setOnAction(new EventHandler<ActionEvent>(){
-		// 	public void handle(ActionEvent action){
-		// 		String newText = text1.getText() + " : from the center text field!";
-		// 		text2.setText(newText);
-		// 		button1.setDisable(true);
-		// 		button1.setText("pressed");
-		// 	}
-		// });
-
-
-		//asas
-		// VBox bottomBox = new VBox(10, imageView, button2);
+		// ---- MAIN LAYOUT ----
+		VBox mainBox = new VBox(20, textBox, imageView, buttonBox);
+		mainBox.setAlignment(Pos.CENTER);
+		mainBox.setPadding(new Insets(20));
 
 		BorderPane borderPane = new BorderPane();
-		borderPane.setStyle("-fx-background-color: #ffffffff;"); 
-		borderPane.setPadding(new Insets(10));
-		borderPane.setTop(textBox);
-		borderPane.setCenter(imageView);
-		borderPane.setBottom(buttonBox);
-		// borderPane.setTop(text2);
-		// borderPane.setLeft(buttonBox);
-
-		// BorderPane.setMargin(buttonBox, new Insets(50));
-        // BorderPane.setMargin(text2, new Insets(50));
-				
 		borderPane.setTop(menuBar);
+		borderPane.setCenter(mainBox);
 
-		Scene scene = new Scene(borderPane, 700,700);
+		Scene scene = new Scene(borderPane, 700, 900);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
-				
 		
 	}
 
