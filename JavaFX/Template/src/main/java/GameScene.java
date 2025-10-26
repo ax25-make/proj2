30aff2f370333cc519366244e0772004cff620c5
Guillermo <ramireziiiguillermo@gmail.com>
2025-10-26 12:17:01 -0500

    Changed for content to be in boarderpane and not vbox

--- JavaFX/Template/src/main/java/GameScene.java
+++ JavaFX/Template/src/main/java/GameScene.java
@@ -1,4 +1,5 @@
 import javafx.animation.PauseTransition;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -19,6 +20,7 @@ public class GameScene {
 
     public GameScene(Stage stage) {
         this.stage = stage;
+        stage.setTitle("Keno Game");
         //game = new KenoGame();
 
         // ---- MENU ----
@@ -41,22 +43,33 @@ public class GameScene {
         menuBar.getMenus().add(menu);
 
         // ---- Bet Card Grid ----
-        // betCardGrid = new GridPane();
-        // betCardGrid.setHgap(5);
-        // betCardGrid.setVgap(5);
-        // betCardGrid.setAlignment(Pos.CENTER);
+        betCardGrid = new GridPane();
+        betCardGrid.setHgap(5);
+        betCardGrid.setVgap(5);
+        betCardGrid.setAlignment(Pos.CENTER);
 
-        // for (int i = 1; i <= 80; i++) {
-        //     Button spot = new Button(String.valueOf(i));
-        //     spot.setDisable(true); // enable later after spot selection
-        //     //spot.setOnAction(e -> selectNumber(spot));
-        //     spot.setMinSize(40, 40);
-        //     betCardGrid.add(spot, (i - 1) % 10, (i - 1) / 10);
-        // }
-        VBox layout = new VBox(15, menuBar);
+        for (int i = 1; i <= 80; i++) {
+            Button spot = new Button(String.valueOf(i));
+            spot.setDisable(true); // enable later after spot selection
+            //spot.setOnAction(e -> selectNumber(spot));
+            spot.setMinSize(40, 40);
+            betCardGrid.add(spot, (i - 1) % 10, (i - 1) / 10);
+        }
+
+        VBox layout = new VBox(30, betCardGrid);
         layout.setAlignment(Pos.TOP_CENTER);
-        layout.setSpacing(15);
-    	scene = new Scene(layout, 700, 900);
+        layout.setStyle(
+            "-fx-padding: 0 0 100 0;"
+        );
+        // layout.setSpacing(15);
+
+        BorderPane borderPane = new BorderPane();
+		borderPane.setStyle("-fx-background-color: #b3b3b3ff;"); 
+		borderPane.setPadding(new Insets(10));
+        borderPane.setTop(menuBar);
+        borderPane.setCenter(layout);
+
+        scene = new Scene(borderPane, 700,900);
     }
 
     public Scene getScene() {
